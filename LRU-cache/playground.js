225c8e26674a1d056edc6b4025e3702d0f741942
Thuong Ho <thuong.t.ho@gmail.com>
2020-04-24 17:14:14 -0700

    add LRU cache

---
+++ LRU-cache/playground.js
@@ -0,0 +1,85 @@
+/**
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LRUCache cache = new LRUCache( 2 // capacity );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+*/
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+  // map to keep track of item to index
+  // array to hold value
+  this.values = [];
+  this.indexMap = new Map();
+  this.capacity = capacity;
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  if (!this.indexMap.has(key)) return -1;
+  const lookUpIndex = this.indexMap.get(key);
+  // reshuffle
+  const renewItem = this.values.splice(lookUpIndex, 1)[0];
+  this.values.push(renewItem);
+  for (let i = lookUpIndex; i < this.values.length; i++) {
+    const key = this.values[i].key;
+    this.indexMap.set(key, i);
+  }
+  return renewItem.value;
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  // build new array on put
+  this.get(key);
+  // capacity reached
+  if (this.indexMap.size === this.capacity) {
+    // remove LRU
+    const leastUsedKey = this.values[0].key;
+    this.indexMap.delete(leastUsedKey);
+    this.values = this.values.slice(1);
+    for (let i = 0; i < this.values.length; i++) {
+      const key = this.values[i].key;
+      this.indexMap.set(key, i);
+    }
+  }
+  // add key value
+  this.indexMap.set(key, this.indexMap.size);
+  this.values.push({ key, value });
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+// ["LRUCache","put","put","get","put","get","put","get","get","get"]
+// [[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
