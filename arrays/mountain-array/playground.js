c529e6601ccd45af5326d2184658d8111e31c040
Thuong Ho <thuong.t.ho@gmail.com>
2020-07-23 18:57:25 -0700

    Add Mountain array

---
+++ arrays/mountain-array/playground.js
@@ -0,0 +1,70 @@
+/**
+Given an array A of integers, return true if and only if it is a valid mountain array.
+
+Recall that A is a mountain array if and only if:
+
+A.length >= 3
+There exists some i with 0 < i < A.length - 1 such that:
+A[0] < A[1] < ... A[i-1] < A[i]
+A[i] > A[i+1] > ... > A[A.length - 1]
+
+Example 1:
+
+Input: [2,1]
+Output: false
+Example 2:
+
+Input: [3,5,5]
+Output: false
+Example 3:
+
+Input: [0,3,2,1]
+Output: true
+ 
+
+Note:
+
+0 <= A.length <= 10000
+0 <= A[i] <= 10000 
+*/
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ */
+// i: number array o: boolean
+// [0,3,2,1]
+var validMountainArray = function (A) {
+  if (!A || A.length < 3) return false;
+
+  // previous number
+  // current number
+  let prevIdx = 0;
+  let prevNum = A[prevIdx];
+  let currIdx = 1;
+  let currNum = A[currIdx];
+  const length = A.length;
+  let slope = 0;
+
+  // loop through array
+  while (currIdx < length) {
+    // plateau return false
+    if (prevNum === currNum) return false;
+    // slope is downward from the start false
+    if (!slope && currNum < prevNum) return false;
+
+    // upward slope
+    // current number > prev
+    if (slope === 0 || slope === 1) {
+      if (currNum < prevNum) {
+        slope = -1;
+      }
+    } else {
+      // downward slope
+      // current number < prev
+      // slope is upward return false
+      if (currNum > prevNum) return false;
+    }
+  }
+
+  // return true
+};
