04f8984fb7853b2706b929a7f8058c29df3653d3
Thuong Ho <thuong.t.ho@gmail.com>
2020-08-10 14:59:52 -0700

    Add dfs inorder traversal

---
+++ trees/bt-inorder-traversal/playground.js
@@ -0,0 +1,38 @@
+/**
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  const final = [];
+  if (!root) return final;
+  const traverse = (node) => {
+    if (node && node.left) traverse(node.left);
+    if (node) final.push(node.val);
+    if (node && node.right) traverse(node.right);
+  };
+  traverse(root);
+  return final;
+};
